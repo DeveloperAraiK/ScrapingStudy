cc9dc067f1e4ca608186d1cc5a4c1b9efeef10e2
AraiK <arai.kenichi11@gmail.com>
2020-03-14 07:58:48 +0900

    コメント追加など

--- assetLogin.py
+++ assetLogin.py
@@ -8,7 +8,7 @@ BASE_URL = "https://moneyforward.com/"
 session = requests.session()
 
 loginUser = input("ID:")
-loginPass = getpass.getpass("PASS:")  # 入力文字をprintしない
+loginPass = getpass.getpass("PASS:")  # 入力Passはprint文出さない
 
 # ログイン情報
 loginInfo = {
@@ -23,19 +23,20 @@ loginInfo = {
 resp = session.get(BASE_URL+"/users/sign_in")
 soup = BeautifulSoup(resp.content,"html.parser")
 
-# print(soup.get_text)
-
+# token取得
+# for key,value in loginInfo.items():
+#     print("{0} {1}".format(key,value))
+ # パスワード、google、Facebookを使ってログインなどがあるので
+ # それぞれにtokenが設定されている。とりあえず全部取り出す
 token = soup.find_all("input",{"name":"authenticity_token"})
+ # パスワードでログインのtokenは1個目
 loginInfo["authenticity_token"] = token[0].get("value")
 
-for key,value in loginInfo.items():
-    print("{0} {1}".format(key,value))
-
-# ログイン先のHTMLを取得
-resLogin = session.post(
+# ログイン後トップページのHTMLを取得
+resTop = session.post(
     BASE_URL+"/session",
     params=loginInfo)
-resLogin.raise_for_status()
+resTop.raise_for_status()
 
-soupLogin = BeautifulSoup(resLogin.text,"html.parser")
-print(soupLogin.get_text)
+soupTop = BeautifulSoup(resTop.text,"html.parser")
+print(soupTop.find_all("a"))
